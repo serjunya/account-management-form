57e292dca6f8dc0bb5c97daa541e910c4bad2ef3
Сергей Головатюков <2003gsa20152@gmail.com>
2026-02-08 01:19:00 +0300

    Revert "Add show/hide password toggle"
    This reverts commit a6c39df83f5e44a6c09759b0da1394eb7b8e4c1b.

--- src/components/AccountRow.vue
+++ src/components/AccountRow.vue
@@ -29,34 +29,16 @@
       @blur="$emit('validate', account.id)"
     />
 
-    <div v-if="account.type === 'LOCAL'" class="password-wrapper">
-      <UITextField
-        v-model="account.password"
-        :name="`password-${account.id}`"
-        :type="account.showPassword ? 'text' : 'password'"
-        placeholder="Введите пароль"
-        :maxlength="100"
-        :invalid="account.errors.password"
-        @blur="$emit('validate', account.id)"
-      />
-      <button
-        class="eye-button"
-        type="button"
-        :aria-label="account.showPassword ? 'Скрыть пароль' : 'Показать пароль'"
-        @click="$emit('toggle-password', account.id)"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          aria-hidden="true"
-        >
-          <path
-            d="M12 5c5.5 0 9.5 4.7 10.6 6.2a1.2 1.2 0 010 1.6C21.5 14.3 17.5 19 12 19S2.5 14.3 1.4 12.8a1.2 1.2 0 010-1.6C2.5 9.7 6.5 5 12 5zm0 2.2c-4 0-7.3 3.4-8.5 4.8 1.2 1.4 4.5 4.8 8.5 4.8s7.3-3.4 8.5-4.8c-1.2-1.4-4.5-4.8-8.5-4.8zm0 2.3a2.5 2.5 0 110 5 2.5 2.5 0 010-5z"
-            fill="currentColor"
-          />
-        </svg>
-      </button>
-    </div>
+    <UITextField
+      v-if="account.type === 'LOCAL'"
+      v-model="account.password"
+      :name="`password-${account.id}`"
+      type="password"
+      placeholder="Введите пароль"
+      :maxlength="100"
+      :invalid="account.errors.password"
+      @blur="$emit('validate', account.id)"
+    />
     <div v-else class="password-placeholder"></div>
 
     <UIButton variant="icon" @click="$emit('remove', account.id)">🗑</UIButton>
@@ -81,7 +63,6 @@ export type LocalAccount = {
   type: 'LDAP' | 'LOCAL' | '';
   login: string;
   password: string;
-  showPassword: boolean;
   errors: AccountErrors;
 };
 
@@ -93,7 +74,6 @@ defineEmits<{
   (event: 'remove', id: string): void;
   (event: 'validate', id: string): void;
   (event: 'type-change', id: string): void;
-  (event: 'toggle-password', id: string): void;
 }>();
 </script>
 
@@ -107,30 +87,6 @@ defineEmits<{
 
 .password-placeholder {
   height: 40px;
-}
-
-.password-wrapper {
-  display: flex;
-  align-items: center;
-  gap: 8px;
-}
-
-.eye-button {
-  width: 36px;
-  height: 36px;
-  border-radius: 8px;
-  border: 1px solid #cbd5f5;
-  background: #fff;
-  cursor: pointer;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  color: #5b6b7f;
-}
-
-.eye-button svg {
-  width: 18px;
-  height: 18px;
 }
 
 @media (max-width: 900px) {
